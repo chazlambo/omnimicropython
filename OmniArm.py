7df284c83d17373c4cef81cd0fb3f1ebc6aa83a8
ashakn65 <ashkan.pourkand@gmail.com>
2021-04-01 19:31:25 -0600

    FK works now

--- OmniArm.py
+++ OmniArm.py
@@ -1,65 +1,58 @@
-class ARM:
-    def __init__(self, ADC_pose, ADC_temp):
-        self.pose = ADC_pose
-        self.temp = ADC_pose
-        self.HT = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-        self.calibrate = False
-        self.mapping = False
+import math
 
-    def GetPose(): # TODO
-        if not calibrate:
-            print("Arm is not calibrated. Set the limits")
-            return None
-        if not mapping:
-            print("Missing mapping limits.")
-            return None
-        self.baseReading = ADC_pose[0]
-        self.vertReading = ADC_pose[1]
-        self.horzReading = ADC_pose[2]
-        self.armReading = ADC_pose[3]
-        
-        theta1 = self.mapping(self.baseReading, self.baseMin, self.baseMax, self.slewMin, self.slewMax)
-        d1 = self.mapping(self.vertReading, self.vertMin, self.vertMax, self.heightMin, self.heightMax)
-        d3 = self.mapping(self.horzReading, self.horzMin, self.horzMax, self.jutMin, self.jutMax)
-        theta4 = self.mapping(self.armReading, self.armMin, self.armMax, self.swivelMin, self.swivelMax)
-        
-        self.HT[0][0] = math.cos(theta1)*math.cos(theta4) - math.sin(theta1)*sin(theta4)
-        self.HT[0][1] = -math.cos(theta1)*math.sin(theta4) - math.sin(theta1)*math.cos(theta4)
-        self.HT[0][3] = d3*math.cos(theta1)
-        self.HT[1][0] = math.sin(theta1)*math.sin(theta4) + math.cos(theta1)*math.sin(theta4)
-        self.HT[1][1] = -math.sin(theta4)*math.sin(theta1) + math.cos(theta1)*math.cos(theta4)
-        self.HT[1][3] = d3*math.sin(theta1)
-        self.HT[2][2] = 1
-        self.HT[2][3] = d1;
-        self.HT[3][2] = 1
-        
-        return None
+class ARM:
+    pose = None
+    temp = None
+    calibrate = False
+    mapping = False
     
-    def SetLimits(self, base_Max, base_Min, vert_Max, vert_Min, horz_Max, horz_Min, wrist_Max, wrist_Min):
-        self.baseMax = base_Max
-        self.baseMin = base_Min
-        self.vertMax = vert_Max
-        self.vertMin = vert_Min
-        self.horzMax = horz_Max
-        self.horzMin = horz_Min
-        self.wristMax = wrist_Max
-        self.wristMin = wrist_Min
+    pose_read = None
+    temp_read = None
+    
+    signalLimits = [] #in order of base>vertical>horizontal>wrist
+    mechLimits = []   #in order of slew>height>jut>swivel
+    
+    def __init__(self, ADC_pose, ADC_temp):
+        self.pose_read = ADC_pose
+        self.temp_read = ADC_temp
+            
+    def SetLimits(self, baseMin, baseMax, vertMin, vertMax, horzMin, horzMax, wristMin, wristMax):
+        self.signalLimits = [baseMin, baseMax, vertMin, vertMax, horzMin, horzMax, wristMin, wristMax]
         self.calibrate = True
         self.mapping = False
-        
+                
     def SetMapping(self, slewMin, slewMax, heightMin, heightMax, jutMin, jutMax, swivelMin, swivelMax):
-        self.slewMin = slewMin
-        self.slewMax = slewMax
-        self.heightMin = heightMin
-        self.heightMax = heightMax
-        self.jutMin = jutMin
-        self.jutMax = jutMax
-        self.swivelMin = swivelMin
-        self.swivelMax = swivelMax
+        self.mechLimits = [slewMin, slewMax, heightMin, heightMax, jutMin, jutMax, swivelMin, swivelMax]
         self.mapping = True
-        
-    def mapping(x, inputMin, inputMax, outputMin, outputMax):
+                    
+    def mapFun(self, x, inputMin, inputMax, outputMin, outputMax):
         return (x - inputMin) * (outputMax-outputMin) + outputMin
     
+    def GetPose(self): # TODO
+        if not self.calibrate:
+            print("Arm is not calibrated. Set the limits")
+            return None
+        if not self.mapping:
+            print("Missing mapping limits.")
+            return None
+        
+        HT = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        theta1 = self.mapFun(self.pose_read[0], self.signalLimits[0], self.signalLimits[1], self.mechLimits[0], self.mechLimits[1])
+        d1 = self.mapFun(self.pose_read[1], self.signalLimits[2], self.signalLimits[3], self.mechLimits[2], self.mechLimits[3])
+        d3 = self.mapFun(self.pose_read[2], self.signalLimits[4], self.signalLimits[5], self.mechLimits[4], self.mechLimits[5])
+        theta4 = self.mapFun(self.pose_read[3], self.signalLimits[6], self.signalLimits[7], self.mechLimits[6], self.mechLimits[7])
+        
+        HT[0][0] = math.cos(theta1)*math.cos(theta4) - math.sin(theta1)*math.sin(theta4)
+        HT[0][1] = -math.cos(theta1)*math.sin(theta4) - math.sin(theta1)*math.cos(theta4)
+        HT[0][3] = d3*math.cos(theta1)
+        HT[1][0] = math.sin(theta1)*math.sin(theta4) + math.cos(theta1)*math.sin(theta4)
+        HT[1][1] = -math.sin(theta4)*math.sin(theta1) + math.cos(theta1)*math.cos(theta4)
+        HT[1][3] = d3*math.sin(theta1)
+        HT[2][2] = 1
+        HT[2][3] = d1
+        HT[3][2] = 1
+
+        return HT
+                        
     def GetTemp(): #TODO
-        return 0
+        return None
--- testFK.py
+++ testFK.py
@@ -1,6 +1,5 @@
 import machine
 import time
-import math
 from OmniArm import *
 
 armInp = 500
@@ -13,17 +12,21 @@ arm1_ADC_temp = [0,0,0]                                      #=PCF8591(i2c_temp,
 arm1 = ARM(arm1_ADC_pose, arm1_ADC_temp)
 print("Checkpoint")
 
-baseMax = 1000
 baseMin = 1
-heightMax = 2000
-heightMin = 2
-horzMax = 3000
+baseMax = 1000
+vertMin = 2
+vertMax = 2000
 horzMin = 3
-armMax = 4000
+horzMax = 3000
 armMin = 4
-print("Checkpoint")
-#arm1.SetLimits(1, 2, 3, 4, 5, 6, 7,8)
-print("Checkpoint")
+armMax = 4000
+
+print("Checkpoint 1")
+
+arm1.SetLimits(baseMin, baseMax, vertMin, vertMax, horzMin, horzMax, armMin, armMax)
+
+print("Checkpoint 2")
+
 slewMin= 0
 slewMax= 2*math.pi
 heightMin= 0.2
@@ -32,7 +35,11 @@ jutMin= 0.1
 jutMax= 0.5
 swivelMin= 0
 swivelMax= 2*math.pi
-arm1.SetMapping(baseMax, baseMin, vertMax, heightMin, horzMax, horzMin, armMax, armMin)
+
+arm1.SetMapping(slewMin, slewMax, heightMin, heightMax, jutMin, jutMax, swivelMin, swivelMax)
+
+print(arm1.calibrate)
+print(arm1.mapping)
 
 
 
